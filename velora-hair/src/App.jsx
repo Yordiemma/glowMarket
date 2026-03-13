8d51a349292e2471ce70b7be059212f0f550c5ac
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-03-13 12:22:21 +0100

    Configure React Router

--- velora-hair/src/App.jsx
+++ velora-hair/src/App.jsx
@@ -1,14 +1,30 @@
 import Homepage from "./pages/Homepage"
+import Shop from "./pages/Shop"
+import MainLayout from "./layout/MainLayout.jsx"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+    ],
+  },
+])
 
 function App() {
-  
-  return (
-    <>
-    <Homepage />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
