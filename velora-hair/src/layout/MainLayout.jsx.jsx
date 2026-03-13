fe18818e80cdb8b5b0ffa8b0ffb9e57d6fb633e9
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-03-13 12:21:08 +0100

    Create project folders: data and layout

---
+++ velora-hair/src/layout/MainLayout.jsx.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import { Outlet } from "react-router-dom"
+
+function MainLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default MainLayout
